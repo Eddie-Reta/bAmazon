e01cadd5d60027faf095f769e2e7d06e81f93995
Hyper_E <er031991@gmail.com>
2019-06-06 23:58:51 -0400

    added to read me

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 # bAmazon
 
+mysql databse 
 ![image](https://user-images.githubusercontent.com/48228824/58730978-9432f000-83bb-11e9-85ea-e928cce40f72.png)
+
+Using inquirer to ask if you want to a buy a product?
+![image](https://user-images.githubusercontent.com/48228824/59080411-94008c00-88b6-11e9-88ee-297e8daedfbc.png)
+
+displaying items from mysql after saying yes
+![image](https://user-images.githubusercontent.com/48228824/59080468-da55eb00-88b6-11e9-9029-e672fabb10b8.png)
+
 
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,7 +1,7 @@
 //npm installed packages for this app to work
-var mysql = require('mysql');
-var inquirer = require('inquirer');
-
+const mysql = require('mysql');
+const inquirer = require('inquirer');
+const { table } = require("table");
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -12,7 +12,7 @@ port : 3306,
 user:"root",
 
 // password for the connection
-password: "",
+password: "Spiderman.3",
 
 //database within mysql workbench 
 database: "bamazon"
@@ -24,40 +24,49 @@ connection.connect( function(err){
         return;
     }
     console.log('connected as id ' + connection.threadId);
-    items();
+   start();
 });
 
 function items(){
     console.log("Here is the list of the items for purchase!")
     var query = "SELECT * FROM products";
     connection.query(query, function(err, res) {
-        if (err) throw err;
+              if (err) throw err;
         for (var i = 0; i < res.length; i++){
-            console.log(res[i].item_id + "   |   " + res[i].product_name + "  |-----DePARTMENT-----|  "+ res[i].department_name + " |-----PRICE-----| " + res[i].price + " |-----AVIALABLE-----| " + res[i].stock_quantity)
+            console.log(res[i].item_id.toString() + " |-----PRODUCT-----|  " + res[i].product_name +  "\n |-----DEPARTMENT-----|  "+ res[i].department_name + "\n |-----PRICE-----|  " + res[i].price + "\n |-----AVIALABLE-----|  " + res[i].stock_quantity)
         }
         console.log('----------------------');
       });
-}
+    }
 
 
+function start(){
+     inquirer
+         .prompt([
+             {
+             name: "question",
+             type: "list",
+             message: "Would you like to buy a product?",
+             choices: [
+                 "Yes", new inquirer.Separator(),
+                 "No"
+             ]
+             }
+            ]).then(answer => {
+                    if(answer.question === "Yes"){
+                  items();}
+               else{ console.log("Fine, Leave you peasent!!");  connection.end()};
+              
+            
 
-// function items(){
-//     inquirer.prompt({
-//             name: "items",
-//             type: "input",
-//             message: "Please choose the ID of the product you would like to purchase?"
-//     }).then(function(){
-//         var query = "SELECT item_id, product_name, department_name, price, stock_quantity FROM products WHERE ?";
-//         connection.query(query);
-//     });
-// }
-
-
-// function start(){
-//     inquirer
-//         .prompt({
-//             name: "Choose id item."
-//             type: ""
-//         })
-// }
-
+       
+       
+       
+       
+                //  const query =  "SELECT item_id, product_name FROM products";
+            //  conection.query(query, function(answer){
+            //      if (err) throw err;
+            //      console.log(result)
+            //  });
+         });
+        }
